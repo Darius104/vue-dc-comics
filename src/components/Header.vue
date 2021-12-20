8b56df32eb95ac77a50696bd950b74dec8b50dfb
Darius104 <comandarius@gmail.com>
2021-12-20 15:38:57 +0100

    Last push

---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header>
+        <div class="container">
+            <div>
+                <img src="../assets/img/favicon.ico" alt="Logo principale">
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header",
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
