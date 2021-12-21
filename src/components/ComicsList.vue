a8f1903a9e3f378eeb6a0e260f22f6dd74a7300d
Darius104 <comandarius@gmail.com>
2021-12-21 17:20:31 +0100

    Last Push

---
+++ src/components/ComicsList.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="back">
+        <div class="container">
+            <SingleComic v-for="(e, index) in list" :key="index" :element = "e"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import './SingleComic.vue';
+export default {
+    name: "ComicsList",
+    components: { SingleComic },
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../style/Commun.scss';
+    .back{
+        background: gray;
+
+        ul{
+            list-style-type: none;
+            display: flex;
+            flex-wrap: wrap;
+
+            li{
+                width: calc(100% / 6);
+                padding: 20px;
+                margin-top: 40px;
+            }
+        }
+    }
+    
+</style>
---
+++ src/components/SingleComic.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div>
+            <img :src="element.thumb" :alt="element.series">
+        </div>
+
+        <div class="title">
+            {{ element.series.toUpperCase() }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'SingleComic',
+    props:['element'],
+}
+</script>
+
+<style lang="scss" scoped>
+    .title{
+        color: white;
+    }
+</style>
